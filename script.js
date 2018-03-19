34fe2b614369f2d1ed0a55a3608d562a50217f3b
Humza Khokhar <lilhumza@gmail.com>
2018-03-19 18:47:44 -0400

    Password Testing

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-var pass;
-var ans = "humza123";
-pass=prompt('Please enter your password to view this page!','');
-var div = document.getElementById('password');
-div.style.visibility = 'hidden';
-if (pass == "humza123"){
-  alert('Password Correct!');
-  div.style.visibility = 'visible';
-} else{ // (pass !== ans) {
-  Window.location="https://www.google.com";
-  HTMLBodyElement.style.visibility ='hidden';
-  div.style.visibility = 'hidden';  
+// var pass;
+// var ans = "humza123";
+// pass=prompt('Please enter your password to view this page!','');
+// var div = document.getElementById('password');
+// div.style.visibility = 'hidden';
+// if (pass == "humza123"){
+//   alert('Password Correct!');
+//   div.style.visibility = 'visible';
+// } else{ // (pass !== ans) {
+//   Window.location="https://www.google.com";
+//   HTMLBodyElement.style.visibility ='hidden';
+//   div.style.visibility = 'hidden';  
 
-  alert('Ba Bye')
-    }
+//   alert('Ba Bye')
+//     }
+function passWord() {
+  var testV = 1;
+  var pass1 = prompt('Please Enter Your Password',' ');
+  while (testV < 3) {
+  if (!pass1) 
+  history.go(-1);
+  if (pass1.toLowerCase() == "humza123") {
+  alert('You Got it Right!');
+  window.open('bonjour.html');
+  break;
+  } 
+  testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+  }
+  if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+  } 
+  
